5c1b31703e05c6382053c6da6cda646d4521d634
Seeya Rani <seeya.rani@duskbyte.com>
2025-04-28 18:30:05 +0530

    Testing

--- api/index.js
+++ api/index.js
@@ -6,9 +6,7 @@
 
 export default async function handler(req, res) {
   // Create query string from request parameters
-  // const queryString = new URLSearchParams(request).toString();
-
-  console.log('Testing');
+  const queryString = new URLSearchParams(request).toString();
 
   res.status(200).json([]);
 
